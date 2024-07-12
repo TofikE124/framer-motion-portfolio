2a4fb358cf2349566bcfcaa8036e42657bc22b8b
TofikE124 <eleltofik@gmail.com>
2024-07-13 02:59:11 +0300

    Add Recent Projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "../components/Hero";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
-  const navItems = [
-    { name: "About", link: "/#about" },
-    { name: "Projects", link: "/#projects" },
-    { name: "Testimonials", link: "/#testimonials" },
-    { name: "Contact", link: "/#contact" },
-  ];
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}></FloatingNav>
         <Hero></Hero>
+        <Grid></Grid>
+        <RecentProjects></RecentProjects>
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,27 +9,7 @@ const Hero = () => {
     <div className="pb-20 pt-36 h-screen">
       <Spotlights></Spotlights>
       <GridDotBackgrounds></GridDotBackgrounds>
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </h2>
-          <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          ></TextGenerateEffect>
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Tofik, a Next.js Developer based in Israel
-          </p>
-          <a href="#about">
-            <MagicButton
-              title="Show my Work"
-              icon={<FaLocationArrow></FaLocationArrow>}
-              position="right"
-            ></MagicButton>
-          </a>
-        </div>
-      </div>
+      <HeroContent></HeroContent>
     </div>
   );
 };
@@ -57,6 +37,32 @@ const Spotlights = () => {
         className="top-28 left-80 h-[80vh] w-[50vw]"
         fill="blue"
       ></Spotlight>
+    </div>
+  );
+};
+
+const HeroContent = () => {
+  return (
+    <div className="flex justify-center relative my-20 z-10">
+      <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
+        <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+          Dynamic Web Magic with Next.js
+        </h2>
+        <TextGenerateEffect
+          words="Transforming Concepts into Seamless User Experiences"
+          className="text-center text-[40px] md:text-5xl lg:text-6xl"
+        ></TextGenerateEffect>
+        <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          Hi, I&apos;m Tofik, a Next.js Developer based in Israel
+        </p>
+        <a href="#about">
+          <MagicButton
+            title="Show my Work"
+            icon={<FaLocationArrow></FaLocationArrow>}
+            position="right"
+          ></MagicButton>
+        </a>
+      </div>
     </div>
   );
 };
